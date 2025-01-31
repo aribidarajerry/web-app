c4fd16909d42cfeb54b30c2adcc580c5fa631c9e
* <aribidarajerry@gmail.com>
2025-01-31 10:11:11 +0100

    Initial commit

--- hello.py
+++ hello.py
@@ -1,73 +1,81 @@
-import math, random, datetime as dt
 
-class Database:
+
+class Users:
     # class variable
     population = 0
-    myDict = {}
+    database = {}
 
     def __init__(self):
-        pass
+        self.my_dict = False
 
-    def organize(self):
-        dash = "-"
-        for key, value in Database.myDict.items():
-            dict_keys = ""
-            for k, v in value.items():
-                dict_keys += f"{k.title():<20}"
-            print(f"{dict_keys}\n{dash*66}") 
-            break
-        for key, value in Database.myDict.items():
-            dict_values = ""
-            for k, v in value.items():
-                dict_values += f"{v:<20}"
-            print(f"{dict_values}\n{dash*66}")  
+    def save(self, data):
+        self.my_dict = data
+        Users.database[Users.population] = data
 
     def admitted(self, firstname):
         verify = False
-        for key, value in Database.myDict.items():
+        for key, value in Users.database.items():
             if (firstname.lower() == value["firstname"]):
+                verify = True
                 return value
         if verify != True:
             return False
+        
+    def register(self, fname, lname, mail, comment):
+        verified = self.admitted(fname)
+        if not verified:
+            Users.population+=1
+            info = {
+                'firstname': fname,
+                'lastname': lname,
+                'mail': mail,
+                'comment': comment
+            }
+            self.save(info)
+        else:
+            print(fname.title() + ' already taken!')
 
+    def update(self, firstname, data, value):
+        verified = self.admitted(firstname)
+        if verified:
+            if data == 'mail' or 'comment':
+                verified[data] = value
+        else:
+            return 'Invalid user!'
+        
     def details(self, firstname, data=False):
-        info = self.admitted(firstname)
-        if info:
-            if (data=='age'):
-                return info[data]
-            elif (data=='gender'):
-                return info[data]
-            else: return info
+        verified = self.admitted(firstname)
+        if verified:
+            if (data=='mail'):
+                return self.my_dict[data]
+            elif (data=='comment'):
+                return self.my_dict[data]
+            else: return self.my_dict
         else:
             return 'Invalid student!'
-
+        
+    def organize(self):
+        dash = "-"
+        for key, value in Users.database.items():
+            dict_keys = ""
+            for k, v in value.items():
+                dict_keys += f"{k.title():<30}"
+            print(f"{dict_keys}\n{dash*66}") 
+            break
+        for key, value in Users.database.items():
+            dict_values = ""
+            for k, v in value.items():
+                dict_values += f"{v:<30}"
+            print(f"{dict_values}\n{dash*66}")  
     
-                
- 
-
-
-class Students(Database):
-
-    def __init__(self, fname, lname, age, gender):
-        super().__init__()
-        Database.population+=1
-        self.firstname = fname
-        self.lastname = lname
-        self.age = age
-        self.gender = gender
-        self.info = {
-            'firstname': fname,
-            'lastname': lname,
-            'age': age,
-            'gender': gender
-        }
-        Database.myDict[Database.population] = self.info 
     
-database = Database()
-student1 = Students('jerry', 'aribidara', 19, 'm')
-student2 = Students('wonder', 'aribidara', 18, 'm')
-student3 = Students('comfort', 'aribidara', 16, 'f')
-student4 = Students('joshua', 'jacob', 20, 'm')
-student5 = Students('queen', 'elizabeth', 20, 'f')
+users = Users()
+users.register('jerry', 'aribidara', 'aribidarajerry@gmail.com', 'm')
+users.register('wonder', 'aribidara', 'aribidarawonder@gmail.com', 'm')
+users.register('comfort', 'aribidara', 'aribidaracomfort@gmail.com', 'f')
+users.register('joshua', 'jacob', 'joshuajacob@gmail.com', 'm')
+users.register('queen', 'elizabeth', 'queenelizabeth@gmail.com', 'f')
+print(users.details('jerry', 'mail'))
+print(users.organize())
 
-print(database.details('queen'))
+
